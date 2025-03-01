cb4e5cbb3b29e1024816c2e8ffe125a8938e475c
Jahongir <alimovj727@gmail.com>
2025-03-01 10:43:34 +0500

    Generate BookResource to format book data with author info

--- app/Http/Resources/AuthorResource.php
+++ app/Http/Resources/AuthorResource.php
@@ -7,13 +7,12 @@ use Illuminate\Http\Resources\Json\JsonResource;
 
 class AuthorResource extends JsonResource
 {
-    /**
-     * Transform the resource into an array.
-     *
-     * @return array<string, mixed>
-     */
+   
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            'name' => $this->name,
+            'bio' => $this->bio,
+        ];
     }
 }
---
+++ app/Http/Resources/BookResource.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class BookResource extends JsonResource
+{
+    public function toArray(Request $request): array
+    {
+        return [
+            'title' => $this->title,
+            'description' => $this->description,
+            'price' => $this->price,
+            'author' => new AuthorResource($this->whenLoaded('author')),
+        ];
+    }
+}
