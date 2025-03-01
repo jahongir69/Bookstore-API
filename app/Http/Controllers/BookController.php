9bd5950d189034c93ce2c49de4bef48c873ac29c
Jahongir <alimovj727@gmail.com>
2025-03-01 10:46:23 +0500

    Fix JSON response format to match requirements

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -2,9 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Book;
 use Illuminate\Http\Request;
+use App\Http\Resources\BookResource;
 
 class BookController extends Controller
 {
-    //
+    public function index()
+    {
+        $books = Book::with('author')->paginate(10);
+
+        return BookResource::collection($books)->response()->getData(true);
+    }
 }
